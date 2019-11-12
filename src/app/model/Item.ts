11dc48575fbc628e7821796d9f45f8b575915e94
superciccio <4390182+superciccio@users.noreply.github.com>
2019-11-12 13:55:34 +0000

    Update Item.ts

--- src/app/model/Item.ts
+++ src/app/model/Item.ts
@@ -1,13 +1,14 @@
 import {Dimension} from './dimension';
 
 export class Item {
-  id: string;
-  price: number;
-  name: string;
-  description: string;
-  images: Array<any>; // link images
-  collectionId: string;
-  categoryItem: string;
-  dimension: Dimension | null;
-  material: string;
+  id: string = "";
+  price: number = 0;
+  name: string = "";
+  description: string = "";
+  images: Array<any> = []; // link images
+  collectionId: string = "";
+  categoryItem: string = "";
+  dimension: Dimension | null = {height=0,depth=0,width=0};
+  material: string = "";
 }
+
