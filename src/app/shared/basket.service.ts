96e51a55ca16bf7ec57c1c246c2079ba43847678
superciccio <4390182+superciccio@users.noreply.github.com>
2019-11-12 14:07:28 +0000

    Update basket.service.ts

--- src/app/shared/basket.service.ts
+++ src/app/shared/basket.service.ts
@@ -7,7 +7,24 @@ import {Item} from '../model/Item';
 export class BasketService {
 
   Items = Array<Item>();
+  order: Order = null;
 
   constructor() {
+    if(order==null){
+      //fill fields
+    this.order = {}
+    }
+  }
+  
+  calculateTotal(): number{
+    let total = 0;
+  for(let f of this.order.furnitures){
+  total += f.price;
+  }
+    return total;
+  }
+  
+  calculateShipmentCost(): number{
+    return 0;
   }
 }
