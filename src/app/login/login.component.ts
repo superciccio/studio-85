41130aa4161075d2e2ec563eafc7414f1a0b7ed6
Andrea Bittau <andrea.bittau@gmail.com>
2019-11-13 20:25:33 +0000

    fix errors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {Router} from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -19,12 +20,12 @@ export class LoginComponent implements OnInit {
   isRecoveryPassword = false;
   isSignup = false;
   isLogin = true;
-  
-  emailCtrl = new FormControl('',[
+
+  emailCtrl = new FormControl('', [
     Validators.required,
     Validators.email
   ]);
-  
+
   /*
   At least 8 characters in length
   Lowercase letters
@@ -32,7 +33,7 @@ export class LoginComponent implements OnInit {
   Numbers
   Special characters
   */
-   pwdCtrl = new FormControl('',[
+   pwdCtrl = new FormControl('', [
     Validators.required,
     Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
   ]);
@@ -42,43 +43,42 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-  
-  backToLogin(){
+
+  backToLogin() {
     this.isLogin = true;
     this.isSignup = false;
     this.isRecoveryPassword = false;
   }
-  
-  signup(){
+
+  signup() {
     this.isLogin = false;
     this.isSignup = true;
     this.isRecoveryPassword = false;
   }
-  
-  recoveryPassword(){
+
+  recoveryPassword() {
     this.isLogin = false;
     this.isSignup = false;
     this.isRecoveryPassword = true;
   }
 
   login() {
-    if(this.email !== '' && this.password !== ''){
+    if (this.email !== '' && this.password !== '') {
         this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then((resp) => {
       this.dialogRef.close();
       this.router.navigate(['/management']);
     }).catch((err) => {
       if (err.code === 'auth/user-not-found') {
         this.userNotFound = true;
-        this.errosMsg = "Email or password do not exists.";
+        this.errosMsg = 'Email or password do not exists.';
 
       }
       if (err.code === 'auth/wrong-password') {
       this.wrongPassword = true;
       this.errosMsg = 'Wrong password. Try again or click Forgot password to reset it.';
-      }
-      else{
+      } else {
       this.wrongPassword = true;
-       this.errosMsg = 'Email or password are wrong.';
+      this.errosMsg = 'Email or password are wrong.';
       }
       console.error(err);
     });
