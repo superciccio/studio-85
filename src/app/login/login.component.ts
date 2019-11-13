b819932079cd0e4cbafbebc154944091dfe89052
superciccio <4390182+superciccio@users.noreply.github.com>
2019-11-13 10:07:28 +0000

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,9 +16,22 @@ export class LoginComponent implements OnInit {
   userNotFound = false;
   wrongPassword = false;
   errosMsg = '';
+  
   emailCtrl = new FormControl('',[
     Validators.required,
     Validators.email
+  ]);
+  
+  /*
+  At least 8 characters in length
+  Lowercase letters
+  Uppercase letters
+  Numbers
+  Special characters
+  */
+   pwdCtrl = new FormControl('',[
+    Validators.required,
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
   ]);
 
   constructor(private dialogRef: MatDialogRef<LoginComponent>, private router: Router, private afAuth: AngularFireAuth) {
@@ -28,27 +41,27 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then((resp) => {
+    if(this.email !== '' && this.password !== ''){
+        this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then((resp) => {
       this.dialogRef.close();
       this.router.navigate(['/management']);
     }).catch((err) => {
       if (err.code === 'auth/user-not-found') {
-      this.userNotFound = true;
+        this.userNotFound = true;
         this.errosMsg = "Email or password do not exists.";
 
-      }if (err.code === 'auth/wrong-password') {
+      }
+      if (err.code === 'auth/wrong-password') {
       this.wrongPassword = true;
-      this.errosMsg = 'Wrong password. \nTry again or click Forgot password to reset it.';
+      this.errosMsg = 'Wrong password. Try again or click Forgot password to reset it.';
+      }
+      else{
+      this.wrongPassword = true;
+       this.errosMsg = 'Email or password are wrong.';
       }
       console.error(err);
     });
-
-    // if (this.password === 'GATTO') {
-    //   this.dialogRef.close();
-    //   this.router.navigate(['/management']);
-    // } else {
-    //   alert('not a valid password ' + this.password);
-    // }
+    }
 
   }
 
