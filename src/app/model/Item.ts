f7d779eeb6ebd708114ffa8b4e1fd9af4c701ee3
superciccio <4390182+superciccio@users.noreply.github.com>
2019-12-03 16:42:11 +0000

    Update Item.ts
    enhanced Item object class

--- src/app/model/Item.ts
+++ src/app/model/Item.ts
@@ -1,14 +1,39 @@
 import {Dimension} from './dimension';
 
+git push gooimport {Dimension} from './dimension';
+
 export class Item {
-  id = '';
-  price = 0;
-  name = '';
-  description = '';
-  images: Array<any> = []; // link images
-  collectionId = '';
-  categoryItem = '';
-  dimension: Dimension | null = {height: 0, depth: 0, width: 0};
-  material = '';
+    id = '';
+    price = 0;
+    name = '';
+    description = '';
+    images: Array<any> = []; // link images
+    collectionId = '';
+    dimension:  Dimension | {height: 0, depth: 0, width: 0};
+    combinations: Array<Combination> = [];
 }
 
+export class Combination {
+    id: string;
+    colour: string | HexColor;
+    style: Style;
+    material: Material;
+    categoryItem: string;
+}
+
+export class HexColor {
+    id: string;
+    value: string;
+}
+
+export class Style {
+    id: string;
+    name: string;
+}
+
+export class Material {
+    id: string;
+    name: string;
+}
+
+gle master
