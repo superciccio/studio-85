0364ba91ac11045b0100536db7be113827d0c20d
superciccio <4390182+superciccio@users.noreply.github.com>
2019-12-03 16:44:19 +0000

    Update furniture.component.ts

--- src/app/furniture/furniture.component.ts
+++ src/app/furniture/furniture.component.ts
@@ -3,6 +3,8 @@ import {SharedVariableService} from '../shared/shared-variable.service';
 import {FurnitureService} from '../shared/furniture.service';
 import {Filter} from '../model/filter';
 import {Router} from '@angular/router';
+import { Item } from '../model/Item';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-furniture',
@@ -11,12 +13,17 @@ import {Router} from '@angular/router';
 })
 export class FurnitureComponent implements OnInit {
   
-  loading: boolean = false;  
+  loading: boolean = false;
+  listFurnitures: Item[] = [];  
   materialFilters: Filter[] = [];
   colourFilters: Filter[] = [];
   styleFilters: Filter[] = [];
   furnitureFilters: Filter[] = [];
 
+  furnitureFilterCtrl = new FormControl();
+  colourFilterCtrl = new FormControl();
+  styleFilterCtrl = new FormControl();
+  materialFilterCtrl = new FormControl();
 
   constructor(private router: Router, private service: SharedVariableService, private fService: FurnitureService) {
     
@@ -24,10 +31,9 @@ export class FurnitureComponent implements OnInit {
 
   ngOnInit() {
     this.loading = true;
-    this.fService.getFilters().toPromise().then(resp => {
-      resp.docs.map(qs => {
+    this.service.getFilters().toPromise().then(resp => {
         
-        for(const doc in qs.documents){
+        for(const doc of resp.docs){
           const element = doc.data() as Filter;
         
           if(element.type == "MATERIALS"){
@@ -49,8 +55,11 @@ export class FurnitureComponent implements OnInit {
         
         }
         
-      } );
     });
+   
+   this.fService.getFurnitures().toPromise().then(resp => {
+       resp.docs.map(qs => this.listFurnitures.push(qs.data() as Item));
+   });
   }
 
   goToDetail(id: string) {
