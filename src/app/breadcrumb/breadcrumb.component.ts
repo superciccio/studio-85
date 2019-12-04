3b85fe4da412f435728cf07853864613e6cf076d
superciccio <4390182+superciccio@users.noreply.github.com>
2019-12-04 12:10:02 +0000

    Update breadcrumb.component.ts

--- src/app/breadcrumb/breadcrumb.component.ts
+++ src/app/breadcrumb/breadcrumb.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class BreadcrumbComponent implements OnInit {
 
   @Input()
-  sections: string[];
+  furniture: Item;
 
   constructor() { }
 
